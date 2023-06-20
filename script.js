8ffe414a62fe75409ca1abd3528edafe2e6dba3f
Artistry449 <artistry449@gmail.com>
2023-06-20 18:00:13 +0800

    Hereglegchees ogogdol avdag bolgoloo

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function () {
-
+    // Өөрчлөлтөнд тэсвэртэй болгох
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+    }
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        // Domstrings-рүү хандаж болох Public Service
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    }
 })();
 // Санхүүтэй ажиллах контроллер
 var financeController = (function () {
@@ -9,11 +28,12 @@ var financeController = (function () {
 // Холбогч контроллер
 var appController = (function (uiController, financeController) {
 
+    var DOM = uiController.getDOMstrings();
     var ctrlAddItem = function () {
         // 1. Оруулах өгөгдлийг дэлгэцээс авна
-
+        console.log(uiController.getInput());
         // 2. Олж авсан өгөгдлөө санхүүгийн контроллер луу дамжуулж тэнд хадгална
-        alert('hahahha');
+
         // 3. Олж авсан өгөгдлүүдээ тохирох хэсэгт нь гаргана
 
         // 4. Төсвийг тооцоолно
@@ -21,7 +41,7 @@ var appController = (function (uiController, financeController) {
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана
     }
 
-    document.querySelector('.add__btn').addEventListener('click', function () {
+    document.querySelector(DOM.addBtn).addEventListener('click', function () {
         ctrlAddItem();
     });
     document.addEventListener('keypress', function (event) {
